f443bbd6f7e045a2adcd6e5d8d92adeb1a8c5981
Alicia Phurisat <aaphurisa@gmail.com>
2018-04-18 13:38:34 -0600

    Created a new project.

---
+++ ConsoleProject1/ConsoleProject1/Program.cs
@@ -0,0 +1,29 @@
+﻿// File/Project Prolog
+// Name:     <PUT YOUR NAME HERE>
+// Period:   <PUT YOUR PERIOD HERE>
+// Username: phuriali000
+// Project:  ConsoleProject1
+// Date:     4/18/2018 1:37:21 PM
+//
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+using System;
+
+namespace ConsoleProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code goes below here
+
+            // Your code goes above here
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
